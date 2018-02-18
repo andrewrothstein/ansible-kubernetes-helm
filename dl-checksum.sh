83567f1e58e6d074f7b073ebf3b47f6775a6294b
Andrew Rothstein <andrew.rothstein@gmail.com>
2018-02-18 01:27:05 -0500

    up to v2.8.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-VER=v2.7.2
+VER=v2.8.1
 MIRROR=https://storage.googleapis.com/kubernetes-helm
 wget $MIRROR/helm-$VER-darwin-amd64.tar.gz
 wget $MIRROR/helm-$VER-linux-amd64.tar.gz
