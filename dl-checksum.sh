699b2cb6c75781fe49784211e81be8735862bace
Andrew Rothstein <andrew.rothstein@gmail.com>
2018-09-16 17:39:46 -0400

    chop cruft

--- dl-checksum.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-VER=v2.9.1
-MIRROR=https://storage.googleapis.com/kubernetes-helm
-wget $MIRROR/helm-$VER-darwin-amd64.tar.gz
-wget $MIRROR/helm-$VER-linux-amd64.tar.gz
-wget $MIRROR/helm-$VER-linux-arm64.tar.gz
-sha256sum helm-$VER-*.tar.gz
-#shasum -a 256 helm-$VER-*.tar.gz
