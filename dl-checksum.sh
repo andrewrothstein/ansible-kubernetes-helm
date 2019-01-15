8d5e1b326988d8561786ebfe43dd31c42b819f68
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-01-14 22:36:13 -0500

    up to v2.12.2. dcb v0.0.16. add fedora_29 CI build. add dl-checksum.sh

---
+++ dl-checksum.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+VER=v2.12.2
+DIR=~/Downloads
+MIRROR=https://storage.googleapis.com/kubernetes-helm
+
+
+dl()
+{
+    OS=$1
+    PLATFORM=$2
+    URL=$MIRROR/helm-$VER-$OS-$PLATFORM.tar.gz.sha256
+    printf "# $URL\n"
+    printf "$OS-$PLATFORM: sha256:"
+    curl -SsL $MIRROR/helm-$VER-$OS-$PLATFORM.tar.gz.sha256
+}
+
+dl linux amd64
+dl linux arm64
+dl darwin amd64
