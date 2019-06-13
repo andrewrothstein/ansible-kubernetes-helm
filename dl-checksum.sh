ed57fbfb04790edb58da851389d379cf1d5100ec
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-06-13 14:15:16 -0500

    up to v2.14.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-VER=v2.14.0
+VER=v2.14.1
 DIR=~/Downloads
 MIRROR=https://storage.googleapis.com/kubernetes-helm
 
@@ -8,12 +8,17 @@ dl()
 {
     OS=$1
     PLATFORM=$2
-    URL=$MIRROR/helm-$VER-$OS-$PLATFORM.tar.gz.sha256
-    printf "# $URL\n"
-    printf "$OS-$PLATFORM: sha256:"
-    curl -SsL $MIRROR/helm-$VER-$OS-$PLATFORM.tar.gz.sha256
+    ARCHIVE_TYPE=$3
+    FILE=helm-$VER-$OS-$PLATFORM.${ARCHIVE_TYPE}.sha256
+    URL=$MIRROR/$FILE
+    printf "# %s\n%s-%s: sha256:%s\n" $URL $OS $PLATFORM `curl -SsL $MIRROR/helm-$VER-$OS-$PLATFORM.tar.gz.sha256`
 }
 
-dl linux amd64
-dl linux arm64
-dl darwin amd64
+dl darwin amd64 tar.gz
+dl linux amd64 tar.gz
+dl linux arm tar.gz
+dl linux arm64 tar.gz
+dl linux 386 tar.gz
+dl linux ppc64le tar.gz
+dl linux s390x tar.gz
+dl windows amd64 zip
