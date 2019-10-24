8228985f7386957afdb115304f91ee1b01932bb2
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-24 12:29:12 -0500

    up to v2.15.1. clean up checksums.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env sh
-VER=v2.15.0
 DIR=~/Downloads
 MIRROR=https://storage.googleapis.com/kubernetes-helm
 
 
 dl()
 {
-    local os=$1
-    local arch=$2
-    local archive_type=$3
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=$4
     local platform="${os}-${arch}"
-    local file=helm-${VER}-${platform}.${archive_type}.sha256
+    local file=helm-${ver}-${platform}.${archive_type}.sha256
     local url=$MIRROR/$file
     printf "    # %s\n"  $url
     printf "    %s: sha256:%s\n" $platform `curl -SsL $url`
 }
 
-printf "  %s:\n" $VER
-dl darwin amd64 tar.gz
-dl linux amd64 tar.gz
-dl linux arm tar.gz
-dl linux arm64 tar.gz
-dl linux 386 tar.gz
-dl linux ppc64le tar.gz
-dl linux s390x tar.gz
-dl windows amd64 zip
+dl_ver () {
+    local ver=$1
+    printf "  %s:\n" $ver
+    dl $ver  darwin amd64 tar.gz
+    dl $ver linux amd64 tar.gz
+    dl $ver linux arm tar.gz
+    dl $ver linux arm64 tar.gz
+    dl $ver linux 386 tar.gz
+    dl $ver linux ppc64le tar.gz
+    dl $ver linux s390x tar.gz
+    dl $ver windows amd64 zip
+}
+
+dl_ver ${1:-v2.15.1}
